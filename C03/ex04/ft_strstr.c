96a4fe15afd359a332d9ed6e1c4604a1b11cdfa4
Test User <test@email.com>
2026-07-29 16:19:26 +0200

    fix

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,15 +6,13 @@ char	*ft_strstr(char *str, char *to_find)
 	i = 0;
 	if (to_find[0] == '\0')
 		return (str);
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		j = 0;
-		while (str[i + j] == to_find[j] && str[i + j] != '\0')
-		{
-			if (to_find[j + 1] == '\0')
-				return (&str[i]);
+		while (str[i + j] && str[i + j] == to_find[j])
 			j++;
-		}
+		if (to_find[j] == '\0')
+			return (&str[i]);
 		i++;
 	}
 	return (0);
