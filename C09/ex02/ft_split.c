60fbc28823203e72447b484233ac967479ee51f2
Test User <test@email.com>
2026-07-30 22:17:20 +0200

    fix

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -1,58 +1,60 @@
 #include <stdlib.h>
 
-int	is_separator(char c, char *charset)
+int	is_sep(char c, char *charset)
 {
 	int	i;
 
 	i = 0;
-	while (charset[i] != '\0')
+	while (charset[i])
 	{
-		if (c == charset[i])
+		if (charset[i] == c)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-int	count_words(char *str, char *charset)
+int	word_count(char *str, char *charset)
 {
 	int	i;
 	int	count;
 
 	i = 0;
 	count = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (str[i] != '\0' && is_separator(str[i], charset))
+		while (str[i] && is_sep(str[i], charset))
 			i++;
-		if (str[i] != '\0')
+		if (str[i])
+		{
 			count++;
-		while (str[i] != '\0' && !is_separator(str[i], charset))
-			i++;
+			while (str[i] && !is_sep(str[i], charset))
+				i++;
+		}
 	}
 	return (count);
 }
 
-int	word_length(char *str, char *charset)
+int	word_len(char *str, char *charset)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && !is_separator(str[i], charset))
+	while (str[i] && !is_sep(str[i], charset))
 		i++;
 	return (i);
 }
 
-char	*copy_word(char *str, int length)
+char	*copy_word(char *str, int len)
 {
 	char	*word;
 	int		i;
 
-	word = malloc(sizeof(char) * (length + 1));
-	if (word == NULL)
+	word = malloc(len + 1);
+	if (!word)
 		return (NULL);
 	i = 0;
-	while (i < length)
+	while (i < len)
 	{
 		word[i] = str[i];
 		i++;
@@ -63,27 +65,27 @@ char	*copy_word(char *str, int length)
 
 char	**ft_split(char *str, char *charset)
 {
-	char **result;
+	char **split;
 	int i;
 	int j;
 
-	result = malloc(sizeof(char *) * (count_words(str, charset) + 1));
-	if (result == NULL)
+	split = malloc(sizeof(char *) * (word_count(str, charset) + 1));
+	if (!split)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (str[i] != '\0' && is_separator(str[i], charset))
+		while (str[i] && is_sep(str[i], charset))
 			i++;
-		if (str[i] != '\0')
+		if (str[i])
 		{
-			result[j] = copy_word(&str[i], word_length(&str[i], charset));
+			split[j] = copy_word(&str[i], word_len(&str[i], charset));
 			j++;
+			while (str[i] && !is_sep(str[i], charset))
+				i++;
 		}
-		while (str[i] != '\0' && !is_separator(str[i], charset))
-			i++;
 	}
-	result[j] = NULL;
-	return (result);
+	split[j] = NULL;
+	return (split);
 }
